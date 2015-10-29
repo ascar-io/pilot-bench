e12791bbf7224d7b62062cfbbf0122836db28c87
Yan Li <yanli@soe.ucsc.edu>
2015-10-29 14:50:17 -0700

    [README] List our interesting features

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # INTRODUCTION
 
 Pilot is a tool and a library (libpilot) that help you run benchmarks
-to get scientifically accurate and meaningful results.
+to get scientifically accurate and meaningful results. Specifically, it
+provides the following features:
+
+*  deciding the optimal length for running a benchmark session so that the results can be bounded by a certain confidence interval,
+*  handling the correlation between test runs,
+*  saving test results in a standard format for sharing and comparing results.
 
 # INSTALL
 
@@ -11,20 +16,25 @@ We plan to support all flavors of UNIX/Linux as the first stage, and
 hopefully Windows and other embedded systems in the future.
 
 We have tested it on the following operating systems:
+
 1. Mac OS X Version 10.11 El Capitan
 2. Ubuntu 15.10 amd64
 
 ## Requirements
 
-Pilot requires a modern C++ compiler, CMake, and the boost C++
-library.
+Pilot requires a modern C++ compiler, CMake, and the boost C++ library.
+
+It also uses gtest (Google Test) for running test cases, but can install gtest automatically when you run ``make'' for the first time.
 
 ## Steps
+
+Install the dependencies as listed above, then:
 
 1. make build
 2. cd build
 3. cmake ..
-4. make test (run all the test cases)
+4. make
+5. make test (run all the test cases)
 
 # DEVELOPMENT
 
---
+++ lib/test/unit_test_statistics.cc
@@ -0,0 +1,62 @@
+/*
+ * unit_test_statistics.cc: unit tests for statistics routines
+ *
+ * Copyright (c) 2015, University of California, Santa Cruz, CA, USA.
+ *
+ * Developers:
+ *   Yan Li <yanli@cs.ucsc.edu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Storage Systems Research Center, the
+ *       University of California, nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * REGENTS OF THE UNIVERSITY OF CALIFORNIA BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <algorithm>
+#include "gtest/gtest.h"
+#include "libpilot.h"
+#include <vector>
+
+using namespace boost::accumulators;
+using namespace std;
+using namespace std::placeholders;
+
+/**
+ * \details These sample response time are taken from Domenic Ferrari's
+ * Computer Systems Performance Evaluation, page 79.
+ */
+const vector<double> g_mean_response_time{
+    1.21, 1.67, 1.71, 1.53, 2.03, 2.15, 1.88, 2.02, 1.75, 1.84, 1.61, 1.35, 1.43, 1.64, 1.52, 1.44, 1.17, 1.42, 1.64, 1.86, 1.68, 1.91, 1.73, 2.18,
+    2.27, 1.93, 2.19, 2.04, 1.92, 1.97, 1.65, 1.71, 1.89, 1.70, 1.62, 1.48, 1.55, 1.39, 1.45, 1.67, 1.62, 1.77, 1.88, 1.82, 1.93, 2.09, 2.24, 2.16
+};
+
+TEST(StatisticsUnitTest, Mean) {
+    accumulator_set< double, features<tag::mean > > acc;
+    for_each(g_mean_response_time.begin(), g_mean_response_time.end(), bind<void>( ref(acc), _1 ) );
+    ASSERT_DOUBLE_EQ(mean(acc), 1.756458333333333) << "Mean is wrong";
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
