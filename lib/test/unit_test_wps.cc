cfd70286f619232caebf5a99431ecc35dc96b255
Yan Li <yanli@soe.ucsc.edu>
2016-07-01 18:49:40 -0700

    Added missing unit_test_wps.cc

---
+++ lib/test/unit_test_wps.cc
@@ -0,0 +1,81 @@
+/*
+ * unit_test_wps.cc
+ * Unit tests for WPS-related functions
+ *
+ * Copyright (c) 2016, University of California, Santa Cruz, CA, USA.
+ * Created by Yan Li <yanli@ascar.io>,
+ * Department of Computer Science, Baskin School of Engineering.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Storage Systems Research Center, the
+ *       University of California, nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * REGENTS OF THE UNIVERSITY OF CALIFORNIA BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "gtest/gtest.h"
+#include "pilot/libpilot.h"
+
+using namespace pilot;
+using namespace std;
+
+nanosecond_type const ONE_SECOND = 1000000000LL;
+size_t const g_max_work_amount = 2000;
+
+int mock_workload_func(const pilot_workload_t *wl,
+                       size_t round,
+                       size_t total_work_amount,
+                       pilot_malloc_func_t *lib_malloc_func,
+                       size_t *num_of_work_unit,
+                       double ***unit_readings,
+                       double **readings,
+                       nanosecond_type *round_duration, void *data) {
+    if (g_max_work_amount == total_work_amount) {
+        // make it a valid round when the work_amount == max_work_amount
+        *round_duration = 2 * ONE_SECOND;
+    }
+    if (5 == round)
+        return 1;
+    else
+        return 0;
+}
+
+TEST(WPSUnitTest, CornerCases) {
+    pilot_set_log_level(lv_no_show);
+    shared_ptr<pilot_workload_t> wl(pilot_new_workload("WPSUnitTest"), pilot_destroy_workload);
+    // WPS is not possible when init_work_amount == max_work_amount
+    pilot_set_init_work_amount(wl.get(), g_max_work_amount);
+    pilot_set_work_amount_limit(wl.get(), g_max_work_amount);
+    ASSERT_EQ(ERR_WRONG_PARAM, pilot_set_wps_analysis(wl.get(), NULL, true, true)); // This function needs to fail
+    // We work around the safety check.
+    pilot_set_init_work_amount(wl.get(), g_max_work_amount / 4);
+    pilot_set_workload_func(wl.get(), mock_workload_func);
+    pilot_set_wps_analysis(wl.get(), NULL, true, true);
+    // Set short round threshold to 1 s. Our mock_work_func will not reach 1 s
+    // until work_amount == max_work_amount. This renders WPS analysis
+    // impossible.
+    pilot_set_short_round_detection_threshold(wl.get(), 1);
+    pilot_run_workload(wl.get());
+    // Make sure WPS has no result
+    shared_ptr<pilot_analytical_result_t> ar(pilot_analytical_result(wl.get(), NULL), pilot_free_analytical_result);
+    ASSERT_EQ(false, ar->wps_has_data);
+}
