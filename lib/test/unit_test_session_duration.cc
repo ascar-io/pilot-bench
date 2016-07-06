7ef556a2aba20af3f2aec168a6410202650dc691
Yan Li <yanli@soe.ucsc.edu>
2016-07-06 13:38:28 -0700

    [unit_test_session_duration.cc] Fixed test case to account for round_work_amount_to_avg_amount_limit_

--- lib/test/unit_test_session_duration.cc
+++ lib/test/unit_test_session_duration.cc
@@ -32,6 +32,9 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// This test cases needs DEBUG
+#undef NDEBUG
+
 #include <algorithm>
 #include <cstring>
 #include "gtest/gtest.h"
@@ -44,8 +47,8 @@ using namespace std;
 
 static size_t g_work_amount_limit;
 static const int g_num_of_pi = 1;
-static vector<int> *g_expected_work_amount_per_round = NULL;
-static vector<int> *g_required_sample_size_per_round = NULL;
+static const vector<size_t> *g_expected_work_amount_per_round = NULL;
+static const vector<int> *g_required_sample_size_per_round = NULL;
 
 int mock_workload_func(const pilot_workload_t *wl,
                        size_t round,
@@ -56,6 +59,9 @@ int mock_workload_func(const pilot_workload_t *wl,
                        double **readings,
                        nanosecond_type *round_duration, void *data) {
     assert(g_work_amount_limit >= work_amount);
+    assert(g_expected_work_amount_per_round);
+    assert((*g_expected_work_amount_per_round)[round] == work_amount);
+
     *num_of_work_unit = work_amount;
 
     // store unit_readings
@@ -87,26 +93,26 @@ ssize_t mock_calc_required_ur_func(const pilot_workload_t* wl, int piid) {
 }
 
 void test_opt_session_duration(size_t work_amount_limit,
-                               vector<int> expected_work_amount_per_round,
-                               vector<int> required_sample_size_per_round) {
-    pilot_workload_t *wl = pilot_new_workload("Test workload");
-    pilot_set_short_workload_check(wl, false);
-    pilot_set_workload_func(wl, &mock_workload_func);
-    pilot_set_work_amount_limit(wl, work_amount_limit);
-    pilot_set_num_of_pi(wl, 1);
-    pilot_set_pi_info(wl, 0, "TestPI", "tick", NULL, NULL,
+                               const vector<size_t> &expected_work_amount_per_round,
+                               const vector<int> &required_sample_size_per_round) {
+    shared_ptr<pilot_workload_t> wl(pilot_new_workload("Test workload"), pilot_destroy_workload);
+    pilot_set_short_workload_check(wl.get(), false);
+    pilot_set_workload_func(wl.get(), &mock_workload_func);
+    pilot_set_work_amount_limit(wl.get(), work_amount_limit);
+    pilot_set_num_of_pi(wl.get(), 1);
+    pilot_set_pi_info(wl.get(), 0, "TestPI", "tick", NULL, NULL,
                       false,  /* reading must satisfy */
                       true);  /* unit readings must satisfy */
-    pilot_set_calc_required_unit_readings_func(wl, &mock_calc_required_ur_func);
-    pilot_set_wps_analysis(wl, NULL, false, false);
-    pilot_set_short_round_detection_threshold(wl, 0);
-    pilot_set_warm_up_removal_method(wl, NO_WARM_UP_REMOVAL);
+    pilot_set_calc_required_unit_readings_func(wl.get(), &mock_calc_required_ur_func);
+    pilot_set_wps_analysis(wl.get(), NULL, false, false);
+    pilot_set_short_round_detection_threshold(wl.get(), 0);
+    pilot_set_warm_up_removal_method(wl.get(), NO_WARM_UP_REMOVAL);
 
     g_expected_work_amount_per_round = &expected_work_amount_per_round;
     g_required_sample_size_per_round = &required_sample_size_per_round;
     g_work_amount_limit = work_amount_limit;
 
-    int res = pilot_run_workload(wl);
+    int res = pilot_run_workload(wl.get());
     if (0 != res) {
         cerr << "pilot_run_workload() returned " << res << endl;
         abort();
@@ -116,25 +122,32 @@ void test_opt_session_duration(size_t work_amount_limit,
         cerr << __func__ << "(): not enough rounds" << endl;
         abort();
     }
-    pilot_destroy_workload(wl);
+
+    g_expected_work_amount_per_round = NULL;
+    g_required_sample_size_per_round = NULL;
 }
 
 TEST(PilotRunWorkloadTest, CalculatingOptimalSessionDuration) {
-    // Tests if pilot_run_workload() can correctly assign work_amount for each
-    // round. In the following case, the work_amount_limit is set to 50. The
-    // first round's work amount is expected to be
-    // expected_work_amount_per_round[0]=5 (1/10 of work_amount_limit). Our
-    // mock workload func would return 5 samples (sample to work amount ratio
-    // is 1:1 here.) The test case uses the calc_readings_required_func hook to tell
-    // pilot_run_workload() that a total of 15 samples are required (as in
-    // required_sample_size_per_round[0]). Now we have 5 samples so 10 more samples
-    // are required. We expect the next round to have 10*(1+.2)=12 samples (as in
-    // expected_work_amount_per_round[1], .2 is the ratio of safety buffer).
+    // Tests if pilot_run_workload() can correctly assign work_amount
+    // for each round. In the following case, the work_amount_limit is
+    // set to 50. The first round's work amount
+    // (expected_work_amount_per_round[0]) is expected to be 1 (trial
+    // run). Our mock workload func would return 1 sample (sample to
+    // work amount ratio is 1:1 here.) The test case uses the
+    // calc_readings_required_func hook to tell pilot_run_workload()
+    // that a total of 15 samples are required (as in
+    // required_sample_size_per_round[0]). Now we have 1 sample so 14
+    // more samples are required. We expect the next round to have
+    // 14*(1+.2)=12 samples (as in expected_work_amount_per_round[1],
+    // .2 is the ratio of safety buffer), but
+    // round_work_amount_to_avg_amount_limit_ defaults to 5 so the
+    // work amount limit for next round is 1*5 = 5. We need 15-1-5=9
+    // samples for the third round, and trunc(9*1.2) = 10.
     pilot_set_log_level(lv_warning);
     test_opt_session_duration(
             50,              // work_amount_limit
-            { 5, 12},        // expected_work_amount_per_round
-            {15, 15, 15});   // required_sample_size_per_round
+            { 1,  5, 10},    // expected_work_amount_per_round
+            {15, 15, 15, 15});   // required_sample_size_per_round
     //! TODO: a test case whose average num of work units per work amount != 1
     //! TODO: a test case for multiple PIs that each of which has a different work_unit_per_amount ratio
 }
