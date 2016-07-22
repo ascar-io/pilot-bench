bc823f690454e3cf213eead7ac1a1a5ebe65e3b6
Yan Li <yanli@ascar.io>
2016-07-21 21:31:46 -0700

    New example benchmark_dd_no_wps/run_dd_extract_tp.sh

---
+++ examples/benchmark_dd_no_wps/run_dd_extract_tp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Run dd and extract duration from dd's output
+# Author: Yan Li <yanli@ascar.io>
+# This file is in public domain.
+set -e -u
+
+if [ $# -lt 2 ]; then
+    cat<<EOF
+Usage: $0 output_file io_count
+output_file:   the file to write to
+io_count:      how many MBs to write
+EOF
+    exit 2
+fi
+
+OUTPUT_FILE=$1
+IO_COUNT=$2
+
+dd if=/dev/zero of=$OUTPUT_FILE bs=1m count=$IO_COUNT 2>&1 | \
+    grep "bytes transferred" | \
+    sed "s/^.*secs (\([\.0-9][\.0-9]*\) bytes.*/\1/g"
+
