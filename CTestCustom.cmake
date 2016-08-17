1d059cb38d100741d9ce75ddaec05e3ba41b75d5
Yan Li <yanli@ascar.io>
2016-08-16 19:05:35 -0700

    Ignore bash scripts when running dynamic memory analyzer

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,2 +1,9 @@
 # We don't want to valgrind the python interpreter
 set (CTEST_CUSTOM_MEMCHECK_IGNORE ${CTEST_CUSTOM_MEMCHECK_IGNORE} unit_test_python_statistics)
+# Ignore test cases the heavily use bash
+set (CTEST_CUSTOM_MEMCHECK_IGNORE ${CTEST_CUSTOM_MEMCHECK_IGNORE}
+     unit_test_analyze
+     unit_test_benchmark
+     unit_test_lua_prompt
+     unit_test_pilot_opt_parsing
+    )
